4e4ff7c88efc458a255fcc9ab9aad7ac26dc2902
Ian <connellian@yahoo.com>
2023-03-17 10:43:40 -0400

    cahgnes s3 bucket acl

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -1,4 +1,9 @@
 resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket = "pipeline-artifacts-ian-420"
-  acl    = "private"
 } 
+
+
+resource "aws_s3_bucket_acl" "codepipeline_artifacts_acl" {
+  bucket = aws_s3_bucket.codepipeline_artifacts.id
+  acl    = "private"
+}
