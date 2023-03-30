11956c87fe4564f47d1829e75070b5f9e0cf91ec
Ian <connellian@yahoo.com>
2023-03-30 14:45:12 -0400

    changed the codestart source repo

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -69,7 +69,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             version = "1"
             output_artifacts = ["tf-code"]
             configuration = {
-                FullRepositoryId = "connellian/aws-cicd-pipeline"
+                FullRepositoryId = "connellian/EKS-Terraform"
                 BranchName   = "master"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
